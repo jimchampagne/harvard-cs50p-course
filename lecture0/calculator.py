16875844545acd73e7b2d0c7bf9b408ae4adf9c1
Jim Champagne <jim.chmpgn@gmail.com>
2024-07-16 18:43:09 +0200

    Calculations

---
+++ lecture0/calculator.py
@@ -0,0 +1,19 @@
+# Integers
+x = int(input("What is x? "))
+y = int(input("What is y? "))
+
+print(x + y)
+
+# Floats
+x = float(input("What is x? "))
+y = float(input("What is y? "))
+
+print(x + y)
+
+# Round
+x = float(input("What is x? "))
+y = float(input("What is y? "))
+
+z = round(x + y, 3)
+
+print(f"{z:,}")
